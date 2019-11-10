cc100b7d69c56c8d526acf2178eb9b9084cbc6af
bestloup <colaslou559@gmail.com>
2019-11-10 19:59:21 +0100

    Version 20H 19/11
    version  début de travail en groupe

--- src/app/sqlite/sqlite.page.ts
+++ src/app/sqlite/sqlite.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+
+
+const DATABASE_FILE_NAME: string = 'data.db';
+
 @Component({
   selector: 'app-sqlite',
   templateUrl: './sqlite.page.html',
@@ -7,8 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SqlitePage implements OnInit {
 
-  constructor() { }
+  private db: SQLiteObject;
 
+  constructor(
+    private sqlite: SQLite
+    ) {
+
+  }
+
+  private createDatabaseFile(): void {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+
+      db.executeSql('create table danceMoves(name VARCHAR(32))')
+      .then(() => console.log('Executed SQL'))
+      .catch(e => console.log(e));
+    })
+    .catch(e => console.log(e));
+
+  }
   ngOnInit() {
   }
 
