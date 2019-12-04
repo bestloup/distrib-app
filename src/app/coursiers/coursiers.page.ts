6218680359466e0184935d7588ff5cfce2765fb7
bestloup <colaslou559@gmail.com>
2019-12-04 10:14:01 +0100

    geo est un essai pour la carte+geolocalisation

--- src/app/coursiers/coursiers.page.ts
+++ src/app/coursiers/coursiers.page.ts
@@ -44,4 +44,37 @@ export class CoursiersPage {
        console.log('Error getting location', error);
      });
   }
+
+  /* idee2() {
+    ngOnInit() {
+      this.leafletMap();
+    }
+
+    ngAfterViewInit(): void {
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+        //console.log('latitude : ' + this.latitude + ' ,  longitude : ' + this.longitude);
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+  }
+
+    leafletMap() {
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+        //console.log('latitude : ' + this.latitude + ' ,  longitude : ' + this.longitude);
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+      console.log(this.latitude);
+      console.log(this.longitude);
+      this.map = new Map('mapId').setView([45, 4.5], 13);
+
+      tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(this.map);
+
+    }
+  }
+*/
 }
