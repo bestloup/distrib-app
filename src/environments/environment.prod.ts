73187bc7e2494277ae4e8645edbcc33c7026066c
bestloup <colaslou559@gmail.com>
2019-12-11 15:23:30 +0100

    après reda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyCwFA7XoLzpEN_7xXBbxLRTeKI7npoi-8k",
-    authDomain: "distrib-90510.firebaseapp.com",
-    databaseURL: "https://distrib-90510.firebaseio.com",
-    projectId: "distrib-90510",
-    storageBucket: "distrib-90510.appspot.com",
-    messagingSenderId: "137201211555",
-    appId: "1:137201211555:web:779ab42b18fa4bffef0716",
-    measurementId: "G-T0JR5YSGLZ"
+    apiKey: "AIzaSyCibpbR9EAak8VGlPOz1LESXUnVzwwMTdg",
+    authDomain: "distrib2-e52f9.firebaseapp.com",
+    databaseURL: "https://distrib2-e52f9.firebaseio.com",
+    projectId: "distrib2-e52f9",
+    storageBucket: "distrib2-e52f9.appspot.com",
+    messagingSenderId: "972364094992",
+    appId: "1:972364094992:web:3f371cba1071bd6fbe7495",
+    measurementId: "G-DLE7QT7HGV"
   }
 };
