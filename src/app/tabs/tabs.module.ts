1750620421776ef34a29c4121cd699e8f92337c1
Dorgone <38913668+Dorgone@users.noreply.github.com>
2020-01-19 14:08:08 +0100

    Les tabs fonctionnent pour client ET marchand

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,19 +1,20 @@
-import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { TabsPageRoutingModule } from './tabs-routing.module';
-
 import { TabsPage } from './tabs.page';
+import { TabsmarchandPage } from '../tabsmarchand/tabsmarchand.page'; //
+import { TabsmarchandPageRoutingModule } from '../tabsmarchand/tabsmarchand-routing.module'; //
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    TabsmarchandPageRoutingModule
   ],
-  declarations: [TabsPage]
+  declarations: [TabsPage, TabsmarchandPage] //
 })
 export class TabsPageModule {}
